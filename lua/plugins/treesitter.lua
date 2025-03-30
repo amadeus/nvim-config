4a03376980be977ed9e54010e6ad100eba931c79
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-03-30 12:19:14 -0700

    Fix treesitter config
    Looks like my opts stuff was too aggressive.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,41 +1,47 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "c",
-      "css",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "query",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-      disable = {},
-    },
-    -- incremental_selection = {enable = true},
-    textobjects = { enable = true },
-    -- autotag = {
-    --   enable = true
-    -- }
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ignore_install = {},
+      modules = {},
+      auto_install = true,
+      sync_install = true,
+      ensure_installed = {
+        "c",
+        "css",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+        disable = {},
+      },
+      -- incremental_selection = {enable = true},
+      textobjects = { enable = true },
+      -- autotag = {
+      --   enable = true
+      -- }
 
-    -- playground = {
-    --   enable = true,
-    --   disable = {},
-    --   updatetime = 25,
-    --   persist_queries = false,
-    -- }
-  },
+      -- playground = {
+      --   enable = true,
+      --   disable = {},
+      --   updatetime = 25,
+      --   persist_queries = false,
+      -- }
+    })
+  end,
 }
