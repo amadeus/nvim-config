2eb941b430dbd530b89563ff273772acec0c8554
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-03-31 17:16:56 -0700

    Hmmm lol

--- lua/plugins/avant.lua
+++ lua/plugins/avant.lua
@@ -1,12 +1,21 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  version = false,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     "saghen/blink.cmp",
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
   },
   build = "make",
   opts = {
