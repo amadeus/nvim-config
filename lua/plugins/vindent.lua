a46cdfdbde6aa339ffb19c9ab094f273d20fbc53
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-04-01 15:53:07 -0700

    Adding indent plugin...
    I found I relied on this so heavily, that I needed it back...

---
+++ lua/plugins/vindent.lua
@@ -0,0 +1,11 @@
+return {
+  "jessekelighine/vindent.nvim",
+  config = function()
+    local keymap_set = function(lhs, rhs)
+      vim.keymap.set({ "x", "o" }, lhs, rhs)
+    end
+    keymap_set("ii", "<Plug>(VindentObject_XX_ii)")
+    keymap_set("ai", "<Plug>(VindentObject_XX_ai)")
+    keymap_set("aI", "<Plug>(VindentObject_XX_aI)")
+  end,
+}
