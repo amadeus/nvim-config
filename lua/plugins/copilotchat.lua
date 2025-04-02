c6d1b6fcc6cad119dc290eda39b98920a5c516d8
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-04-02 10:18:25 -0700

    Adding copilot chat...
    Seems better than the hacky claude chat thing from before

---
+++ lua/plugins/copilotchat.lua
@@ -0,0 +1,9 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+    { "nvim-lua/plenary.nvim", branch = "master" },
+  },
+  build = "make tiktoken",
+  opts = {},
+}
