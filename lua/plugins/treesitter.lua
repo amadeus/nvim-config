0519e5f8ac38af66730ea23385563a84f1e12484
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-04-06 12:03:35 -0700

    Smol papercut fix with markdown bullet wrapping

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,62 +2,54 @@ return {
   "nvim-treesitter/nvim-treesitter",
   version = false,
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ignore_install = {},
-      modules = {},
-      auto_install = true,
-      sync_install = true,
-      ensure_installed = {
-        "c",
-        "css",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
+  opts = {
+    ignore_install = {},
+    modules = {},
+    auto_install = true,
+    sync_install = true,
+    ensure_installed = {
+      "c",
+      "css",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "query",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+      disable = { "markdown" },
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<leader><leader>i",
+        node_incremental = "<leader>l",
+        node_decremental = "<leader>h",
       },
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<leader><leader>i",
-          node_incremental = "<leader>l",
-          node_decremental = "<leader>h",
-        },
-      },
-      textobjects = {
-        enable = true,
-      },
-      matchup = {
-        enable = true,
-      },
-      fold = {
-        enable = true,
-      },
-      -- autotag = {
-      --   enable = true
-      -- }
-
-      -- playground = {
-      --   enable = true,
-      --   disable = {},
-      --   updatetime = 25,
-      --   persist_queries = false,
-      -- }
-    })
+    },
+    textobjects = {
+      enable = true,
+    },
+    matchup = {
+      enable = true,
+    },
+    fold = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
     vim.opt.foldlevel = 99
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
