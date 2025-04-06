f8a184ce04f035ddc23a54359a30c06dca5b4a77
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-04-06 00:28:08 -0700

    Smol dream feature for Wipeout
    I've kinda always wanted the ability to wipeout all but the buffer I'm
    in, and this lil luascript modification is now back in action
    
    I also added an All variant to keep the old functionality if I wanted
    it... lets see how this goes

--- lua/config/wipeout.lua
+++ lua/config/wipeout.lua
@@ -1,7 +1,24 @@
-local function wipeout_buffers()
+local function wipeout_all_buffers()
   vim.cmd("%bwipeout")
 end
 
+local function wipeout_buffers()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local all_bufs = vim.api.nvim_list_bufs()
+  for _, buf in ipairs(all_bufs) do
+    if buf ~= current_buf then
+      -- Check if the buffer is valid before attempting to wipe it
+      if vim.api.nvim_buf_is_valid(buf) then
+        -- Use pcall to catch any errors that might occur when trying to wipe a buffer
+        pcall(function()
+          vim.cmd("bwipeout! " .. buf)
+        end)
+      end
+    end
+  end
+end
+
 vim.api.nvim_create_user_command("Wipeout", wipeout_buffers, {})
+vim.api.nvim_create_user_command("WipeoutAll", wipeout_all_buffers, {})
 
 return {}
