3cf5114db548541c15be9fe27c491be393777427
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-04-08 11:16:45 -0700

    Have wipeout still clean startify buffers

--- lua/config/wipeout.lua
+++ lua/config/wipeout.lua
@@ -6,7 +6,8 @@ local function wipeout_buffers()
   local current_buf = vim.api.nvim_get_current_buf()
   local all_bufs = vim.api.nvim_list_bufs()
   for _, buf in ipairs(all_bufs) do
-    if buf ~= current_buf then
+    -- Skip current buffer only if it's not startify
+    if buf ~= current_buf or vim.bo[current_buf].filetype == "startify" then
       -- Check if the buffer is valid before attempting to wipe it
       if vim.api.nvim_buf_is_valid(buf) then
         -- Use pcall to catch any errors that might occur when trying to wipe a buffer
