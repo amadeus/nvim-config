770bd89f28467b1de44df5177f741e5ef55168f1
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-04-13 17:07:57 -0700

    Allow conform auto format to be toggle-able

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,11 @@
-local function get_format_options(bufnr)
+local function get_format_options(bufnr, ignore_disable)
+  local is_disabled = not ignore_disable
+    and (vim.b[bufnr].conform_disable_formatting == true or vim.g.conform_disable_formatting == true)
+
+  if is_disabled then
+    return nil
+  end
+
   bufnr = bufnr or 0
   local default_config = vim.g.project_format_options or nil
 
@@ -34,10 +41,33 @@ return {
 
     -- Formatting (equivalent to ALEFix)
     vim.keymap.set("n", "<leader>ff", function()
-      local config = get_format_options(vim.api.nvim_get_current_buf())
+      local config = get_format_options(vim.api.nvim_get_current_buf(), true)
       if config then
         conform.format(config)
       end
     end, { desc = "Format document" })
+
+    -- Disable local buffer formatting on save
+    vim.keymap.set("n", "<leader>ft", function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      if vim.b[bufnr].conform_disable_formatting == true then
+        vim.b[bufnr].conform_disable_formatting = nil
+        vim.notify("Auto formatting on save enabled for this buffer")
+      else
+        vim.b[bufnr].conform_disable_formatting = true
+        vim.notify("Auto formatting on save disabled for this buffer")
+      end
+    end, { desc = "Toggle format on save for current buffer" })
+
+    -- Disable buffer format on save globally
+    vim.keymap.set("n", "<leader>fT", function()
+      if vim.g.conform_disable_formatting == true then
+        vim.g.conform_disable_formatting = nil
+        vim.notify("Auto formatting on save enabled globally")
+      else
+        vim.g.conform_disable_formatting = true
+        vim.notify("Auto formatting on save disabled globally")
+      end
+    end, { desc = "Toggle format on save globally" })
   end,
 }
