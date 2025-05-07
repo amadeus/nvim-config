c92feba5cd3ddcc3d31a2a7fc3da4a844f9d4d66
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-05-07 10:26:52 -0700

    Use the new gemini model

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -88,7 +88,8 @@ return {
           schema = {
             model = {
               -- default = "gemini-2.5-pro-exp-03-25",
-              default = "gemini-2.5-pro-preview-03-25",
+              -- default = "gemini-2.5-pro-preview-03-25",
+              default = "gemini-2.5-pro-preview-05-06",
             },
           },
         })
