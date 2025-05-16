b01ec38cb091d12ae9f88b6cb641c164b647038a
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-05-15 22:42:24 -0700

    Fix vindent configuration
    There was a fairly major update, that actually started doing the logic
    that I wanted, I just needed to configure it properly, so here we are!

--- lua/plugins/vindent.lua
+++ lua/plugins/vindent.lua
@@ -1,12 +1,22 @@
 return {
   "jessekelighine/vindent.nvim",
   version = false,
-  config = function()
-    local keymap_set = function(lhs, rhs)
-      vim.keymap.set({ "x", "o" }, lhs, rhs)
-    end
-    keymap_set("ii", "<Plug>(VindentObject_XX_ii)")
-    keymap_set("ai", "<Plug>(VindentObject_XX_ai)")
-    keymap_set("aI", "<Plug>(VindentObject_XX_aI)")
+  opts = {
+    begin = true,
+    noisy = false,
+    infer = false,
+  },
+  config = function(_, opts)
+    local vindent = require("vindent")
+    local block_opts = {
+      strict = { skip_empty_lines = false, skip_more_indented_lines = false },
+      contiguous = { skip_empty_lines = false, skip_more_indented_lines = true },
+      loose = { skip_empty_lines = true, skip_more_indented_lines = true },
+    }
+    vindent.map.Object("iI", "ii", block_opts.loose)
+    vindent.map.Object("ii", "ii", block_opts.contiguous)
+    vindent.map.Object("ai", "ai", block_opts.loose)
+    vindent.map.Object("aI", "aI", block_opts.loose)
+    vindent.setup(opts)
   end,
 }
