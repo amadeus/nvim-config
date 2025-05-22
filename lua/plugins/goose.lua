f95ab7275b6bad94bf7c93925cef9bd69c058378
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-05-21 23:26:48 -0700

    goose.vim still broken

--- lua/plugins/goose.lua
+++ lua/plugins/goose.lua
@@ -1,10 +1,14 @@
 return {
   "azorng/goose.nvim",
-  branch = "main",
+  version = false,
   -- Currently not super happy with this because it appears to lack an easy way
   -- to customize the popups that it creates. Also fwiw, I am a bit scared that
   -- this agent can just go ham and do stuff you may not want it to do.  It
   -- could be powerful at some point tho...
+  -- Played with it again -- still has issues
+  --  * UI sucks -- why can't it be a split? Why is word wrap off by default?
+  --  * Why can't it access files I specify? It claims they are blank...
+  --  (perhaps a goose misconfiguration)
   enabled = false,
   opts = {
     keymap = {
@@ -15,6 +19,9 @@ return {
         close = "<leader>cq",
         toggle_fullscreen = "<leader>cf",
         select_session = "<leader>cs",
+      },
+      window = {
+        submit = "<c-cr>",
       },
     },
   },
