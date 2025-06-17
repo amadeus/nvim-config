c77daa8dfa3be99cc6c337771d61439e079d2576
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-06-17 11:34:46 -0700

    Add snacks.vim
    Also just add a quick config for big files, definitely fixed some shit
    for me...

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = {
+      enabled = true,
+      size = 0.5 * 1024 * 1024, -- 0.5mb
+      notify = true,
+      line_length = 1000,
+    },
+  },
+}
