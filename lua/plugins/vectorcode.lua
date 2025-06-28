74acce5a0ea0f9345cfbcc2d38741168ed03c781
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-06-28 12:23:52 -0700

    Experimented with vectorcode, but like... it's a hassle

---
+++ lua/plugins/vectorcode.lua
@@ -0,0 +1,9 @@
+return {
+  "Davidyz/VectorCode",
+  enabled = false,
+  version = "*", -- optional, depending on whether you're on nightly or release
+  cmd = "VectorCode", -- if you're lazy-loading VectorCode
+  opts = {
+    async_backend = "lsp",
+  },
+}
