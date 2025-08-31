8c9a8b0f9973ac977e565070a3ba5bd28482a33c
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-08-31 02:19:54 -0700

    ui: misc tweaks to spinners

--- lua/config/lualine-ai-spinner.lua
+++ lua/config/lualine-ai-spinner.lua
@@ -1,6 +1,5 @@
-local spinner_symbols = { "𜶫", "𜷚", "𜷣", "𜷥", "𜷤", "𜷠", "𜷊", "𜵰" }
-
 local Spinner = require("lualine.component"):extend()
+local spinner_symbols = { "𜶫", "𜷚", "𜷣", "𜷥", "𜷤", "𜷠", "𜷊", "𜵰" }
 
 Spinner.processing = false
 Spinner.spinner_index = 1
@@ -26,7 +25,7 @@ end
 function Spinner:update_status()
   if self.processing then
     self.spinner_index = (self.spinner_index % #spinner_symbols) + 1
-    return "🧠" .. spinner_symbols[self.spinner_index]
+    return "🧠 " .. spinner_symbols[self.spinner_index]
   else
     return nil
   end
