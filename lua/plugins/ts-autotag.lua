773a77c9919e5b8c1cf4c46b9253fdfcfd5f6602
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-09-29 12:02:16 -0700

    ts-autotag: adding a rename tag command helper

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -9,4 +9,13 @@ return {
       enabled = false,
     },
   },
+  config = function(_, opts)
+    require("ts-autotag").setup(opts)
+
+    vim.keymap.set("n", "<leader>rt", function()
+      if not require("ts-autotag").rename() then
+        vim.lsp.buf.rename()
+      end
+    end, { desc = "ts-autotag: rename tag" })
+  end,
 }
