d08c13568b8fbcb80c5f9b645af76750c31b7b62
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-11-02 22:40:56 -0800

    misc: move treesitter mods into my repo

---
+++ after/queries/typescript/highlights.scm
@@ -0,0 +1,54 @@
+;; TypeScript Function Parentheses Highlights
+;; Place this file in ~/.config/nvim/after/queries/typescript/highlights.scm
+
+;; Regular function declarations
+(function_declaration
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+;; Function expressions
+(function_expression
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+;; Arrow functions
+(arrow_function
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+;; Method definitions in classes
+(method_definition
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+;; Function types in type annotations
+(function_type
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+;; Method signatures in interfaces
+(method_signature
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+;; Call signatures in interfaces
+(call_signature
+  parameters: (formal_parameters
+    "(" @function.paren.open
+    ")" @function.paren.close))
+
+
+
+;; If you're using React/TSX, add this for function components
+;; (jsx_element
+;;   open_tag: (jsx_opening_element
+;;     name: (identifier) @_name
+;;     (#match? @_name "^[A-Z]"))
+;;   (#has-parent? function_declaration function_expression arrow_function)
+;; )
