d5a582de1b690d99ea44e7fce1c4c502c8ac70ad
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-02-28 13:13:59 -0800

    treesitter: update to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   version = false,
-  branch = "master",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  opts = {
-    ignore_install = {},
-    modules = {},
-    auto_install = true,
-    sync_install = true,
-    ensure_installed = {
+  config = function()
+    require("nvim-treesitter").setup()
+
+    -- Install commonly used parsers
+    require("nvim-treesitter").install({
       "c",
       "css",
       "fish",
@@ -17,7 +17,6 @@ return {
       "javascript",
       "jsdoc",
       "json",
-      "jsonc",
       "lua",
       "luadoc",
       "markdown",
@@ -28,31 +27,33 @@ return {
       "vim",
       "vimdoc",
       "yaml",
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-      disable = { "markdown" },
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<leader><leader>i",
-        node_incremental = "<leader>l",
-        node_decremental = "<leader>h",
-      },
-    },
-    textobjects = {
-      enable = true,
-    },
-    fold = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    })
+
+    -- Enable treesitter highlighting for all filetypes with a parser
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        -- Only start if a parser exists for this filetype
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if lang and vim.treesitter.language.add(lang) then
+          vim.treesitter.start(args.buf)
+        end
+      end,
+    })
+
+    -- Enable treesitter indentation (experimental), except for markdown
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function(args)
+        if args.match == "markdown" then
+          return
+        end
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if lang and vim.treesitter.language.add(lang) then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+
+    -- Folding
     vim.opt.foldlevel = 99
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
