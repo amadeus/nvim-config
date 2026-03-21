437ab4c5dc8ab68205ef1688a91f4a787a4d14d1
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-03-21 15:24:28 -0700

    misc: cleanup

--- lua/plugins/vectorcode.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Davidyz/VectorCode",
-  enabled = false,
-  version = false, -- optional, depending on whether you're on nightly or release
-  cmd = "VectorCode", -- if you're lazy-loading VectorCode
-  opts = {
-    async_backend = "lsp",
-  },
-}
