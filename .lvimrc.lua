e6447091da64bd308819ec636767ed29b811595c
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-03-21 15:26:21 -0700

    misc: disable startify in this buffer

--- .lvimrc.lua
+++ .lvimrc.lua
@@ -7,6 +7,7 @@ vim.g.localvimrc_sourced_once = 1
 
 vim.cmd("echom 'nvim-config .lvimrc has been sourced'")
 
+vim.g.startify_disable_at_vimenter = 1
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
