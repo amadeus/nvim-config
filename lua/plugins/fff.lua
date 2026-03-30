92d69cb6c349707cae4c066b5ae1e59374435d29
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-03-30 11:53:32 -0700

    fff: download binaries instead of trying to build
    It's caused me too many problems, done with this

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,7 +1,30 @@
 return {
   "dmtrKovalenko/fff.nvim",
+  -- version = "*",
   version = false,
-  build = "cargo build --release",
+  build = function()
+    local done = false
+    local download_error = nil
+
+    require("fff.download").download_binary(function(success, err)
+      if not success then
+        download_error = err or "unknown error"
+      end
+      done = true
+    end)
+
+    local ok, wait_err = vim.wait(1000 * 60 * 2, function()
+      return done
+    end, 100)
+
+    if not ok and wait_err == -2 then
+      error("fff.nvim: download_binary timed out")
+    end
+
+    if download_error then
+      error("Failed to download fff.nvim binary: " .. download_error)
+    end
+  end,
   opts = {
     prompt = " ",
     preview = {
