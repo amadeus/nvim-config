1e2636e1992eaab85e64c7e889b05ec32c887ec7
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-04-02 23:12:03 -0700

    flog: Fix fugitive dep

--- lua/plugins/vim-flog.lua
+++ lua/plugins/vim-flog.lua
@@ -2,7 +2,9 @@ return {
   "rbong/vim-flog",
   lazy = true,
   cmd = { "Flog", "Flogsplit", "Floggit" },
-  dependencies = { "tpope/vim-fugitive", version = false },
+  dependencies = {
+    { "tpope/vim-fugitive", version = false },
+  },
   config = function()
     vim.g.flog_default_opts = {
       date = "format:%Y-%m-%d %H:%M:%S",
