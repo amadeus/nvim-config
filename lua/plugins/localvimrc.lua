dbe12dee0d565ee693a6d7ed34146385067af2b3
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-04-23 12:28:14 -0700

    localvimrc: disable sandboxing (it doesn't work with lua)

--- lua/plugins/localvimrc.lua
+++ lua/plugins/localvimrc.lua
@@ -2,7 +2,8 @@ return {
   "embear/vim-localvimrc",
   version = false,
   init = function()
-    vim.g.localvimrc_sandbox = 1
+    -- This doesn't really work with lua code
+    -- vim.g.localvimrc_sandbox = 1
     vim.g.localvimrc_persistent = 1
     vim.g.localvimrc_name = { ".lvimrc", ".lvimrc.lua" }
   end,
