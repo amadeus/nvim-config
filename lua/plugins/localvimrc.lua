b4b8c276cd4394eaf095c65ab378f74f7e4b5d6d
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-04-26 17:28:25 -0700

    localvimrc: some stupid shit that probably doesn't matter...

--- lua/plugins/localvimrc.lua
+++ lua/plugins/localvimrc.lua
@@ -2,8 +2,8 @@ return {
   "embear/vim-localvimrc",
   version = false,
   init = function()
-    -- This doesn't really work with lua code
-    -- vim.g.localvimrc_sandbox = 1
+    -- Sandboxing doesn't really work with lua code
+    vim.g.localvimrc_sandbox = 0
     vim.g.localvimrc_persistent = 1
     vim.g.localvimrc_name = { ".lvimrc", ".lvimrc.lua" }
   end,
