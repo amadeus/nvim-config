e01315b365a5b7142967f6a670e949b70cb09a9a
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-05-01 17:24:47 -0700

    exrc: add :E shortcut for :ExrcLoad

--- lua/plugins/exrc.lua
+++ lua/plugins/exrc.lua
@@ -11,4 +11,10 @@ return {
     },
     trust_on_write = true,
   },
+  config = function(_, opts)
+    require("exrc").setup(opts)
+    vim.api.nvim_create_user_command("E", function(cmd_opts)
+      vim.cmd("ExrcLoad" .. (cmd_opts.bang and "!" or ""))
+    end, { bang = true, desc = "ExrcLoad shortcut" })
+  end,
 }
