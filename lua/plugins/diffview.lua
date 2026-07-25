1f803d0f2a17f453cae5af36bd9bfffbda106aa5
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-07-25 16:24:07 -0700

    diffview: trying out diffview...

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+  "dlyongemallo/diffview-plus.nvim",
+  version = false,
+  keys = {
+    { "<leader>dvo", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+    { "<leader>dvc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    { "<leader>dvh", "<cmd>DiffviewFileHistory %<cr>", desc = "Current file history" },
+    { "<leader>dvH", "<cmd>DiffviewFileHistory<cr>", desc = "Repository history" },
+  },
+}
