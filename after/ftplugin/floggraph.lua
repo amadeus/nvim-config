fb3daf888443ce44307e0090cbb5295005ef6a3f
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-08-31 11:34:42 -0700

    ftplugins: a bit of ftplugin polish
    * remove some filetypes for pluggins i don't use anymore...
    * remove softwrap from markdown
    * add cursorline to floggraph (makes it easier to read)

--- after/ftplugin/floggraph.lua
+++ after/ftplugin/floggraph.lua
@@ -1,3 +1,6 @@
+vim.opt_local.cursorline = true
+vim.opt_local.cursorlineopt = "both"
+
 for _, lhs in ipairs({ "y<C-G>", "y<C-T>", "y<C-X>" }) do
   vim.keymap.del({ "n", "v" }, lhs, { buffer = true })
 end
