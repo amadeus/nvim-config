882d1cfff82269d3cac22090ee83dee7a58cceda
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-09-12 13:48:09 -0700

    config_cleanup(32): bring back lualine update rate
    so spinners will feel better

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -364,6 +364,11 @@ return {
       theme = "tokyonight-night",
       icons_enabled = true,
       always_show_tabline = false,
+      refresh = {
+        statusline = 100,
+        tabline = 100,
+        winbar = 100,
+      },
     },
     sections = default_sections,
     inactive_sections = default_inactive,
