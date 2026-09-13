da694c89c3aed2f620c7e9b9f7a372a53d9451e2
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-09-12 18:08:01 -0700

    help: remove stupid bindings

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,12 +1,3 @@
 if vim.bo.buftype == "help" then
   vim.opt_local.signcolumn = "no"
 end
-
-local opts = { buf = 0, noremap = true }
-
-vim.keymap.set("n", "<CR>", "<C-]>", opts)
-vim.keymap.set("n", "<BS>", "<C-T>", opts)
-vim.keymap.set("n", "o", [[/'\l\{2,\}'<CR>]], opts)
-vim.keymap.set("n", "O", [[?'\l\{2,\}'<CR>]], opts)
-vim.keymap.set("n", "s", [[/\|\zs\S\+\ze\|<CR>]], opts)
-vim.keymap.set("n", "S", [[?\|\zs\S\+\ze\|<CR>]], opts)
