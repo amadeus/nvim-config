511b46e779dc52971a0954186f82c748057f64fc
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-09-12 18:53:26 -0700

    profiling: improve to support lua stuff

--- lua/config/profiling.lua
+++ lua/config/profiling.lua
@@ -1,16 +1,32 @@
--- Note sure if this profiling is even applicable in a Neovim world...
+local profiling = false
+
 local function profile_start()
+  if profiling then
+    vim.notify("Profiling is already running", vim.log.levels.WARN)
+    return
+  end
+
+  local profiler = require("jit.p")
+  -- Sample Lua stacks and VM states every 1 ms, retaining all sampled entries.
+  profiler.start("vl10pri1m0", "profile-lua.log")
   vim.cmd([[
     profile start profile.log
     profile func *
     profile file *
   ]])
+  profiling = true
 end
 
 local function profile_end()
+  if not profiling then
+    vim.notify("Profiling is not running", vim.log.levels.WARN)
+    return
+  end
+
+  require("jit.p").stop()
+  profiling = false
   vim.cmd([[
-    profile pause
-    noautocmd qall!
+    profile stop
   ]])
 end
 
