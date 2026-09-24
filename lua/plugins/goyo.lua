1599c94210e5c8056320764b7ac1681a0c4915af
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-09-23 23:11:08 -0700

    goyo: disable statusbar plugin while in goyo

--- lua/plugins/goyo.lua
+++ lua/plugins/goyo.lua
@@ -6,5 +6,16 @@ return {
     vim.g.goyo_margin_top = 5
     vim.g.goyo_margin_bottom = 5
     vim.g.goyo_width = 90
+
+    vim.api.nvim_create_autocmd("User", {
+      group = vim.api.nvim_create_augroup("NvimConfigGoyo", { clear = true }),
+      pattern = "GoyoEnter",
+      desc = "Hide the status column in Goyo padding windows",
+      callback = function()
+        for _, buf in pairs(vim.t.goyo_pads) do
+          vim.wo[vim.fn.bufwinid(buf)].signcolumn = "no"
+        end
+      end,
+    })
   end,
 }
